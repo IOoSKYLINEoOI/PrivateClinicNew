369a797825670beeda2c955af9a53a65837df530
Владислав Карелин <vladislav5656565656@gmail.com>
2024-08-01 00:33:04 +0300

    ver 0.6.1(register)

---
+++ Clinic.Frontend/clinic/app/Models/RegisterUser.ts
@@ -0,0 +1,11 @@
+interface RegisterUser{
+    id: string;
+    firstName: string;
+    lastName: string;
+    fatherName?: string;
+    dateOfBirth: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+    description?: string;
+}
--- Clinic.Frontend/clinic/app/layout.tsx
+++ Clinic.Frontend/clinic/app/layout.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
-import "./globals.css";
-import { Menu } from "antd";
+import { Menu, Button } from "antd";
 import Link from "next/link";
-
-
+import "./globals.css";
+import { useState } from "react";
+import RegisterModal from "./components/RegisterModal";
+import { registerUser, RegisterUserRequest } from "./services/user";
 
 const items = [
   { key: "home", label: <Link href={"/"}>Home</Link> },
@@ -11,28 +14,57 @@ const items = [
   { key: "employees", label: <Link href={"/employees"}>Employees</Link> },
   { key: "receptions", label: <Link href={"/receptions"}>Receptions</Link> },
   { key: "results", label: <Link href={"/results"}>Results</Link> },
-  { key: "users", label: <Link href={"/users"}>Users</Link> }
 ];
+
 export default function RootLayout({
   children,
-}:{
+}: {
   children: React.ReactNode;
 }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+  const handleRegister = async (request: RegisterUserRequest) => {
+    await registerUser(request);
+    closeModalRegister();
+
+    console.log("Регистрация прошла успешно:", request);
+  };
+
+  const showModalRegister = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModalRegister = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <html lang="en">
       <body>
-        <Layout style={{minHeight: "100vh"}}>
+        <Layout style={{ minHeight: "100vh" }}>
           <Header>
-            <Menu 
-              theme="dark" 
-              mode="horizontal" 
-              items ={items} 
-              style={{flex: 1, minWidth: 0}} 
-            />
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                items={items}
+                style={{ flex: 1, minWidth: 0 }}
+              />
+              <Button type="primary" onClick={showModalRegister}>
+                Register
+              </Button>
+            </div>
           </Header>
-          <Content style ={{padding: "0 48px"}}>{children}</Content>
-          <Footer style = {{textAlign: "centre"}}>Clinic</Footer>
+          <Content style={{ padding: "0 48px" }}>{children}</Content>
+          <Footer style={{ textAlign: "center" }}>Clinic</Footer>
         </Layout>
+
+        <RegisterModal 
+          visible={isModalVisible} 
+          onRegister={handleRegister} 
+          onCancel={closeModalRegister} 
+        />
       </body>
     </html>
   );
